da47e7665fcb6521547ad1ee91b246a77edaf5af
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-04-03 22:37:19 -0300

    feat: criação do objeto

--- src/utils/mockup.js
+++ src/utils/mockup.js
@@ -1,49 +1,55 @@
 function mock() {
 
-    let simuladDados = {"columns": [
-        {
-            "nome": "submission_date"
-            },
-        {
-            "nome": "reviewer_id"
-            },
-        {
-            "nome": "product_id"
-            },
-        {
-            "nome": "product_name"
-            },
-        {
-            "nome": "product_brand"
-            },
-        {
-            "nome": "site_category_lv1"
-            },
-        {
-            "nome": "site_category_lv2"
-            },
-        {
-            "nome": "review_title"
-            },
-        {
-            "nome": "overall_rating"
-            },
-        {
-            "nome": "recommend_to_a_friend"
-            },
-        {
-            "nome": "review_text"
-            },
-        {
-            "nome": "reviewer_birth_year"
-            },
-        {
-            "nome": "reviewer_gender"
-            },
-        {
-            "nome": "reviewer_state"
-            }
-    ]}
+    let simuladDados = {
+        "response": {
+            "fileName": "B2W-Reviews01.csv",
+            "fileSize": 57.1047477722168,
+            "columns": [
+                {
+                    "nome": "submission_date"
+                },
+                {
+                    "nome": "reviewer_id"
+                },
+                {
+                    "nome": "product_id"
+                },
+                {
+                    "nome": "product_name"
+                },
+                {
+                    "nome": "product_brand"
+                },
+                {
+                    "nome": "site_category_lv1"
+                },
+                {
+                    "nome": "site_category_lv2"
+                },
+                {
+                    "nome": "review_title"
+                },
+                {
+                    "nome": "overall_rating"
+                },
+                {
+                    "nome": "recommend_to_a_friend"
+                },
+                {
+                    "nome": "review_text"
+                },
+                {
+                    "nome": "reviewer_birth_year"
+                },
+                {
+                    "nome": "reviewer_gender"
+                },
+                {
+                    "nome": "reviewer_state"
+                }
+            ]
+        }
+    }
 
-    localStorage.setItem('dadosUpload',JSON.stringify(simuladDados))
+    localStorage.setItem('dadosUpload', JSON.stringify(simuladDados))
 }
