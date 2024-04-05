5daf9851915591055969843557b036d7ff99c1c8
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-04-05 20:10:01 -0300

    fix:correçoes cod

--- src/utils/mockup.js
+++ src/utils/mockup.js
@@ -12,42 +12,6 @@ function mock() {
                 },
                 {
                     "nome": "reviewer_id"
-                },
-                {
-                    "nome": "product_id"
-                },
-                {
-                    "nome": "product_name"
-                },
-                {
-                    "nome": "product_brand"
-                },
-                {
-                    "nome": "site_category_lv1"
-                },
-                {
-                    "nome": "site_category_lv2"
-                },
-                {
-                    "nome": "review_title"
-                },
-                {
-                    "nome": "overall_rating"
-                },
-                {
-                    "nome": "recommend_to_a_friend"
-                },
-                {
-                    "nome": "review_text"
-                },
-                {
-                    "nome": "reviewer_birth_year"
-                },
-                {
-                    "nome": "reviewer_gender"
-                },
-                {
-                    "nome": "reviewer_state"
                 }
             ]
         }
