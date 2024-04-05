c15a42c68489d8982c3868c4e6ef80efd292c057
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-04-05 16:29:58 -0300

    feat: cronstrução metadados object OK

--- src/utils/mockup.js
+++ src/utils/mockup.js
@@ -1,8 +1,10 @@
 function mock() {
 
     let simuladDados = {
+        
         "response": {
             "fileName": "B2W-Reviews01.csv",
+            "usuario": "eduardo",
             "fileSize": 57.1047477722168,
             "columns": [
                 {
